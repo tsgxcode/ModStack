976ee971a7b37c66532670848339f00c6f6f30f7
TSG <TSG@TSGAC>
2020-09-17 07:07:37 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ModStack
+{
+    class Program
+    {
+        static void Main()
+        {
+            /*Mod Stack - Adding Color modules: Green, Blue, Orange, etc. */
+            Console.WriteLine("Mod Stack: Add color modules...then remove some!");
+
+
+            string block1 = "MossGreen";
+            string block2 = "Cyan";
+            string block3 = "LighGray";
+
+            Console.Write("{3} {0} {1}", block1, block2, block3);
+        }
+    }
+}
