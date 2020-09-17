9a947881e63094684a1db0f0c09eb04c09284301
TSG <TSG@TSGAC>
2020-09-17 07:10:41 -0700

    9.Sept.2020 No bugs, but is throwing an exception...

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ModStack
             /*Mod Stack - Adding Color modules: Green, Blue, Orange, etc. */
             Console.WriteLine("Mod Stack: Add color modules...then remove some!");
 
-
+            //Creating the mods
             string block1 = "MossGreen";
             string block2 = "Cyan";
             string block3 = "LighGray";
